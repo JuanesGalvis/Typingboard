75d75b5b477cd798375ecf3c7b7388af9d4595a0
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2021-10-17 23:31:08 -0500

    Estilos + Captura de tecla presionada

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-const Fila1 = ['Esc', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Impr', 'Bloq Despl', 'Paus']
-const Fila1 = ['°', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '?', '¡', '<-', 'Insert', 'Inicio', 'Re Pág', 'Bloq Num', '/', '*', '-']
-const Fila1 = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '´', '+', 'Enter', 'Supr', 'Fin', 'Av Pág', '7', '8', '9', '+']
-const Fila1 = ['Mayus', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ', '{', '}', '4', '5', '6']
-const Fila1 = ['Shift', '<', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '-', 'Shift', '⬆', '1', '2', '3', 'Intro']
-const Fila1 = ['Ctrl', 'Windows', 'Alt', 'Espacio', 'Alt Gr', 'FN', '🖱', 'Ctrl', '⬅', '⬇', '➡', '0', '.']
+// const Fila1 = ['Escape', 'PrintScreen', 'ScrollLock', 'Pause']
+// const Fila2 = ['|', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '?', '¿', 'Backspace', 'Insert', 'Home', 'PageUp', 'NumLock', '/', '*', '-']
+// const Fila3 = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'Dead', '+', 'Enter', 'Delete', 'End', 'PageDown', '7', '8', '9', '+']
+// const Fila4 = ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ', '{', '}', '4', '5', '6']
+// const Fila5 = ['Shift', '<', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '-', 'Shift', 'ArrowUp', '1', '2', '3', 'Enter']
+// const Fila6 = ['Control', 'Alt', 'Espacio', 'AltGraph', 'ContextMenu', 'Control', 'ArrowLeft', 'ArrowDown', 'ArrowRight', '0', '.']
+
+function Render( keypress ) {
+    
+    document.getElementById('keypress').innerText = keypress;
+    document.getElementById(keypress).classList.add('keypress');
+    setTimeout(() => {
+        document.getElementById(keypress).classList.remove('keypress');
+    }, 500)
+}
+
+function logKeyUp(event) {
+    
+    if (document.getElementById(event.code)) {
+        Render(event.code);
+    } else if (document.getElementById(event.key)) {
+        Render(event.key);
+    }
+
+
+    if (event.key === "ScrollLock") document.querySelector('.keyboard__numbers__leds--scroll .led').classList.toggle('active')
+    if (event.key === "NumLock") document.querySelector('.keyboard__numbers__leds--numbers .led').classList.toggle('active')
+    if (event.key === "CapsLock") document.querySelector('.keyboard__numbers__leds--uppercase .led').classList.toggle('active')
+
+}
+
+document.addEventListener('keyup', logKeyUp);
